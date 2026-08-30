d423cae271141d0569ac90365622b0151857b081
Carolina Santiago <ctt.carolinasantiago@gmail.com>
2026-08-30 19:49:08 -0300

    created atividades 2.1, 2.2 and 2.3

---
+++ Atividade 2/atividade2.1.asm
@@ -0,0 +1,19 @@
+lw x10, a
+lw x11, b
+lw x12, m
+
+bge x11, x12, fim
+add x12, x10, x11
+fim:
+
+blt x11, x12, else
+sub x12, x10, x11
+else:
+
+sw x12, m
+halt
+
+a: .word 0x6
+b: .word 0x0f
+m: .word 0x0000
+
